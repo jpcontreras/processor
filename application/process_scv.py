aae334093b4d99e44c4d67798a86c914d6d077b2
juanpacken <juan.contreras@packen.co>
2021-05-24 14:24:01 -0500

    feature(application/process_csv infraestructure/services/postcodes_service): postcodes service was used to get 100 geolocations every request. Logic created to send geolocations every 100 rows in loop of csv

--- application/process_scv.py
+++ application/process_scv.py
@@ -2,14 +2,17 @@ import csv
 import shutil
 import os
 from infraestructure.services.postcodes_service import PostcodesService
+import json
 
 class ProcessCsv:
+    # Bulk translates geolocations into Postcodes. Accepts up to 100 geolocations.
+    ROW_TO_REQUEST = 100
+
     # ===== Parameters:
     # *+file+:UploadFile
     def __init__(self, file):
         self.file = file
-        self.total_rows = 1
-        self.geolocations = []
+        self.total_rows = 0
 
     def run(self):
         with open(self.file.filename, "wb") as buffer:
@@ -37,30 +40,32 @@ class ProcessCsv:
     # ===== Parameters:
     # *+file_path+:String
     def _file_access(self, file_path):
+        self.total_rows = sum(1 for row in (open(file_path)))
         with open(file_path) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            # csv_total_rows = len(list(csv_reader))
-            # print(f'csv_total_rows ===> {csv_total_rows}')
-
-            rows_to_request = 0
-            # service = PostcodesService
+            row_to_request = 1
+            current_row = 1
+            geolocations = []
+            service = PostcodesService()
             for row in csv_reader:
-                print(f'lat: {row[0]} lng: {row[1]}')
-                if self.total_rows >= 2:
-                    # print(f'rows_to_request ===> {rows_to_request}')
-                    # print(f'total_rows ===> {self.total_rows}')
-                    if rows_to_request == 100: #or self.total_rows == csv_total_rows:
-                        #response = service.bulk_search_nearest(self.geolocations)
-                        print(f'geolocations ===> {self.geolocations}')
-                        self.geolocations = []
-                        rows_to_request = 0
-                    else:
-                        print(f'lat: {row[0]} lng: {row[1]}')
-                        self.geolocations.append({
-                            "longitude": row[1],
-                            "latitude": row[0],
-                            "radius": 1000,
-                            "limit": 1
-                        })
-                        rows_to_request += 1
-                self.total_rows += 1
+                if current_row >= 2:
+                    geolocations.append({
+                        "longitude": float(row[1]),
+                        "latitude": float(row[0]),
+                        "radius": 1000,
+                        "limit": 1
+                    })
+                    # TODO: search and learn about multiprocessing (https://rico-schmidt.name/pymotw-3/multiprocessing/basics.html)
+                    if row_to_request == self.ROW_TO_REQUEST or current_row == self.total_rows:
+                        print(f'Geolocations to send ===> {len(geolocations)}')
+                        response = service.bulk_search_nearest(geolocations)
+                        data_string = json.loads(response)
+                        print('result:', len(data_string['result']))
+                        # print('JSON:', data_string)
+
+                        geolocations = []
+                        row_to_request = 0
+
+                    row_to_request += 1
+
+                current_row += 1
